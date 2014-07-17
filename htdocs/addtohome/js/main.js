343f9408f1a59113b7b54cd7b55995f3dc9e203e
menrva <internal.admin@wearewhatwedo.org>
2014-07-17 15:24:36 +0100

    new iteration with clear option

--- htdocs/addtohome/js/main.js
+++ htdocs/addtohome/js/main.js
@@ -1,14 +1,27 @@
 (function($, ath, undefined) {
 
+  var localInstance;
+
   $(document).ready(function() {
     // use ath alias of global window.addToHomescreen
     // http://cubiq.org/add-to-home-screen
-    addToHomescreen({
+    localInstance = ath({
       // wait 0.5 seconds before showing the add to homescreen message
       startDelay: 0.5,
+      // only ever show the call out once
+      maxDisplayCount: 1,
       // turn debug on to show on every browser
-      debug: false
+      debug: false,
     });
+
+    // TESTING ONLY
+
+    // bind to buttons
+    $('#removecook').click(function(event) {
+      ath.removeSession();
+      event.preventDefault();
+    });
+
   });
 
 })(window.jQuery, window.addToHomescreen, undefined);
