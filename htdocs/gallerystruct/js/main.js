f72c950e5082de42f63f02e4fcd331413bf0bb81
menrva <internal.admin@wearewhatwedo.org>
2014-08-04 18:21:55 +0100

    working on masonry example

--- htdocs/gallerystruct/js/main.js
+++ htdocs/gallerystruct/js/main.js
@@ -1,6 +1,11 @@
 (function($, sfun, undefined) {
 
   $(document).ready(function() {
+  	var msn = new Masonry( '#masonry', {
+      // options
+      columnWidth: 200,
+      itemSelector: '.item'
+    });
   });
 
 })(window.jQuery, window.sfun, undefined);
