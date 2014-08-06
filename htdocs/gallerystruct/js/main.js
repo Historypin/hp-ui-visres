cad6b4edc017e0fef4d40d88f894ba4ce8be4abf
menrva <internal.admin@wearewhatwedo.org>
2014-08-06 22:32:16 +0100

    late night progress on masonry, pulled new version of sfun

--- htdocs/gallerystruct/js/main.js
+++ htdocs/gallerystruct/js/main.js
@@ -1,11 +1,14 @@
 (function($, sfun, undefined) {
 
+  console.log('pre');
   $(document).ready(function() {
-  	var msn = new Masonry( '#masonry', {
-      // options
-      columnWidth: 200,
-      itemSelector: '.item'
-    });
+    console.log('pre masonry');
+  	// var msn = new Masonry( '#masonry', {
+   //    // options
+   //    columnWidth: 300,
+   //    itemSelector: '.cell'
+   //  });
+   //  console.log('called masonry');
   });
 
 })(window.jQuery, window.sfun, undefined);
